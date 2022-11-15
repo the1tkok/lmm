1ae9bfc8541b72a7412e517556c833ab40b41079
the1tkok <the1tkok@gmail.com>
2022-11-14 21:50:59 -0600

    .

--- components/Dashboard/Navigation/Header/Header.js
+++ components/Dashboard/Navigation/Header/Header.js
@@ -8,42 +8,44 @@ const Header = () => {
     <>
       {/*****MOBILE NAVIGATION*****/}
       <aside className={styles.sideBar}></aside>
-      <nav className={styles.navContainer}>
-        <Image className={styles.logo2} src={logo}
-        alt="logo"
-        width={150}
-        height='100%'
-        />
-        <div className={styles.mobileNavContainer}>
-          <span className={styles.dot1}></span>  
-          <span className={styles.dot2}></span>
-          <span className={styles.dot3}></span>
-          <span className={styles.dot4}></span>  
-          <span className={styles.dot5}></span>
-          <span className={styles.dot6}></span>
-          <span className={styles.dot7}></span>  
-          <span className={styles.dot8}></span>
-          <span className={styles.dot9}></span>
-        </div>
-        {/*****DESKTOP NAVIGATION*****/}
-        <ul className={styles.navUl}>
-          <li className={styles.navListMenu}>
-            <Link href={'/'}><a className={styles.viewCurriculumButton}>View Curriculum</a></Link>
-          </li>
-          <li className={styles.navListMenu}>
-            <Link href={'/'}><a className={styles.pricingButton}>Pricing</a></Link>
-          </li>
-          <li className={styles.navListMenu}>
-            <Link href={'/'}><a className={styles.blogButton}>Blog</a></Link>
-          </li>
-          <li className={styles.navListMenu}>
-            <Link href={'/'}><a className={styles.loginButton}>Login</a></Link>
-          </li>
-          <li className={styles.navListMenu}>
-            <Link href={'/'}><a className={styles.registerButton}>Register</a></Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.headerNavContainer}>
+        <nav className={styles.navContainer}>
+          <Image className={styles.logo2} src={logo}
+          alt="logo"
+          width={150}
+          height='100%'
+          />
+          <div className={styles.mobileNavContainer}>
+            <span className={styles.dot1}></span>  
+            <span className={styles.dot2}></span>
+            <span className={styles.dot3}></span>
+            <span className={styles.dot4}></span>  
+            <span className={styles.dot5}></span>
+            <span className={styles.dot6}></span>
+            <span className={styles.dot7}></span>  
+            <span className={styles.dot8}></span>
+            <span className={styles.dot9}></span>
+          </div>
+          {/*****DESKTOP NAVIGATION*****/}
+          <ul className={styles.navUl}>
+            <li className={styles.navListMenu}>
+              <Link href={'/'}><a className={styles.viewCurriculumButton}>View Curriculum</a></Link>
+            </li>
+            <li className={styles.navListMenu}>
+              <Link href={'/'}><a className={styles.pricingButton}>Pricing</a></Link>
+            </li>
+            <li className={styles.navListMenu}>
+              <Link href={'/'}><a className={styles.blogButton}>Blog</a></Link>
+            </li>
+            <li className={styles.navListMenu}>
+              <Link href={'/'}><a className={styles.loginButton}>Login</a></Link>
+            </li>
+            <li className={styles.navListMenu}>
+              <Link href={'/'}><a className={styles.registerButton}>Register</a></Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   )
 }
