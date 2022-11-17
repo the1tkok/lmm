75999fdac8e9ffa11f73d6795bf42d22dd3e2790
the1tkok <the1tkok@gmail.com>
2022-11-17 03:33:44 -0600

    .

--- components/Dashboard/InfoNav/InfoNav.js
+++ components/Dashboard/InfoNav/InfoNav.js
@@ -7,7 +7,7 @@ import Image from 'next/future/image'
 export default function InfoNavBar() {
   return (
       <div className={styles.infoSidebar}>
-          <div className={styles.userAvatar}><Image className={styles.userAvatarIcon} src={Avatar} alt="background image"></Image></div>
+          <div className={styles.userAvatar}><Image className={styles.userAvatarIcon} src={Avatar} alt="background image" width={48} height={48}></Image></div>
           <div className={styles.userSettings}><Image className={styles.settingsIcon} src={Settings} alt="background image"></Image></div>
           <div className={styles.startDashboardTutorial}><Image className={styles.dashboardTutorialIcon} src={Info} alt="background image"></Image></div>
         </div>
