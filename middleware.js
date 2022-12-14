48b4b1804cb2e1ad789aa008253f3c47d4265577
Leonardo <focuux@gmail.com>
2022-12-14 18:21:33 -0400

    redirect from landing to app when logged in

--- middleware.js
+++ middleware.js
@@ -17,5 +17,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/login", "/register"],
+  matcher: ["/dashboard/:path*", "/login", "/register", "/"],
 };
