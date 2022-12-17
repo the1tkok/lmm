e941bd97e2c9fc8ebffb010d99a3d982e040d69b
the1tkok <the1tkok@gmail.com>
2022-12-17 04:33:11 -0600

    updated some dashboard icons

--- components/Dashboard/InfoNav/InfoNav.jsx
+++ components/Dashboard/InfoNav/InfoNav.jsx
@@ -1,15 +1,15 @@
 import styles from './InfoNav.module.css'
 import Avatar from '../../../public/icons/dashboard_icons/avatar.png'
-import Info from '../../../public/icons/dashboard_icons/info.png'
-import Settings from '../../../public/icons/dashboard_icons/settings.png'
+import Info from '../../../public/icons/dashboard_icons/info.svg'
+import Settings from '../../../public/icons/dashboard_icons/settings.svg'
 import Image from 'next/future/image'
 
 export default function InfoNavBar() {
   return (
       <div className={styles.infoSidebar}>
           <div className={styles.userAvatar}><Image className={styles.userAvatarIcon} src={Avatar} alt="background image" width={48} height={48}></Image></div>
-          <div className={styles.userSettings}><Image className={styles.settingsIcon} src={Settings} alt="background image"></Image></div>
-          <div className={styles.startDashboardTutorial}><Image className={styles.dashboardTutorialIcon} src={Info} alt="background image"></Image></div>
+          <div className={styles.userSettings}><Image className={styles.settingsIcon} src={Settings} alt="background image" width={24} height={24}></Image></div>
+          <div className={styles.startDashboardTutorial}><Image className={styles.dashboardTutorialIcon} src={Info} alt="background image" width={24} height={24}></Image></div>
         </div>
   )
 }
