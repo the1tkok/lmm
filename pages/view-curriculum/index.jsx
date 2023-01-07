221f706feb6ebebee29989f34915c3b08b8b6634
the1tkok <the1tkok@gmail.com>
2023-01-07 03:04:57 -0600

    restructured some code on view curricilum page

--- pages/view-curriculum/index.jsx
+++ pages/view-curriculum/index.jsx
@@ -45,32 +45,32 @@ export default function Home() {
       </div>
       <section className={styles.section2}>
         <section className={styles.secFeaturesContainer}>
-      <h3 className={styles.secFeaturesTitle}>Start with the Foundations</h3>
-      <p className={styles.secFeaturesText}>Not ready for acting class yet? Start with the Foundations of acting. This course is a four-part series with 50 videos,
-      dozens of quizzes, automatic grading, and in-depth additional resources to help you become</p>
-      <div className={styles.cardContainer}>
-      <div className={styles.secFeaturesCard1}>
-        <Image className={styles.toolkitIcon} src={Toolkit} alt="toolkit"></Image>
-        <p className={styles.secFeaturesCardTitle}>The Actor&#39;s Toolkit</p>
-        <p className={styles.secFeaturesCardSubTitle}>Where your acting foundation beings.</p>
-      </div>
-      <div className={styles.secFeaturesCard2}>
-        <Image className={styles.tvIcon} src={Tv} alt="commercial workshop"></Image>
-        <p className={styles.secFeaturesCardTitle}>Commercial Workshop</p>
-        <p className={styles.secFeaturesCardSubTitle}>Learn precision acting to get yourself set up for commercial and TV roles.</p>
-      </div>
-      <div className={styles.secFeaturesCard3}>
-        <Image className={styles.toolkitIcon} src={Briefcase} alt="toolkit"></Image>
-        <p className={styles.secFeaturesCardTitle}>Business of You</p>
-        <p className={styles.secFeaturesCardSubTitle}>How to market yourself and manage your acting career.</p>
-      </div>
-      <div className={styles.secFeaturesCard4}>
-        <Image className={styles.toolkitIcon} src={Studio} alt="toolkit"></Image>
-        <p className={styles.secFeaturesCardTitle}>Film & Television</p>
-        <p className={styles.secFeaturesCardSubTitle}>Story telling, human behavior, acting techniques and much more!</p>
-      </div>
-      </div>
-    </section>
+          <h3 className={styles.secFeaturesTitle}>Start with the Foundations</h3>
+          <p className={styles.secFeaturesText}>Not ready for acting class yet? Start with the Foundations of acting. This course is a four-part series with 50 videos,
+          dozens of quizzes, automatic grading, and in-depth additional resources to help you become</p>
+          <div className={styles.cardContainer}>
+            <div className={styles.secFeaturesCard1}>
+              <Image className={styles.toolkitIcon} src={Toolkit} alt="toolkit"></Image>
+              <p className={styles.secFeaturesCardTitle}>The Actor&#39;s Toolkit</p>
+              <p className={styles.secFeaturesCardSubTitle}>Where your acting foundation beings.</p>
+            </div>
+            <div className={styles.secFeaturesCard2}>
+              <Image className={styles.tvIcon} src={Tv} alt="commercial workshop"></Image>
+              <p className={styles.secFeaturesCardTitle}>Commercial Workshop</p>
+              <p className={styles.secFeaturesCardSubTitle}>Learn precision acting to get yourself set up for commercial and TV roles.</p>
+            </div>
+            <div className={styles.secFeaturesCard3}>
+              <Image className={styles.toolkitIcon} src={Briefcase} alt="toolkit"></Image>
+              <p className={styles.secFeaturesCardTitle}>Business of You</p>
+              <p className={styles.secFeaturesCardSubTitle}>How to market yourself and manage your acting career.</p>
+            </div>
+            <div className={styles.secFeaturesCard4}>
+              <Image className={styles.toolkitIcon} src={Studio} alt="toolkit"></Image>
+              <p className={styles.secFeaturesCardTitle}>Film & Television</p>
+              <p className={styles.secFeaturesCardSubTitle}>Story telling, human behavior, acting techniques and much more!</p>
+            </div>
+          </div>
+        </section>
       </section>
       {/*****FOOTER*****/}
       <Footer></Footer>
